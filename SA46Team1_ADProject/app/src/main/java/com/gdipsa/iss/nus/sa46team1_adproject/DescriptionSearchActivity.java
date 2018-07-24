b69883c8c645098f417707835405fe8b5dde4e14
Hendri <hendrisetiawardana@gmail.com>
2018-07-24 13:20:34 +0800

    wired the search by item description
    wired the search by item description

--- SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/DescriptionSearchActivity.java
+++ SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/DescriptionSearchActivity.java
@@ -2,12 +2,61 @@ package com.gdipsa.iss.nus.sa46team1_adproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.SearchView;
 
-public class DescriptionSearchActivity extends AppCompatActivity {
+import com.gdipsa.iss.nus.sa46team1_adproject.Data.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DescriptionSearchActivity extends AppBaseActivity implements SearchView.OnQueryTextListener {
+
+    private RecyclerView mRecyclerViewSearchItemDescription;
+    private DescriptionSearchAdapter adapter;
+    SearchView editsearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_search);
+        setTitle("Search");
+
+        mRecyclerViewSearchItemDescription = findViewById(R.id.recycler_view_search_description_store);
+
+        //Temporary put in data first
+        List<Item> itemList = new ArrayList<Item>();
+
+        Item item1 = new Item("Test1");
+        Item item2 = new Item("Test2");
+        Item item3 = new Item("Test3");
+        itemList.add(item1);
+        itemList.add(item2);
+        itemList.add(item3);
+
+        adapter = new DescriptionSearchAdapter(DescriptionSearchActivity.this, itemList);
+        mRecyclerViewSearchItemDescription.setAdapter(adapter);
+        mRecyclerViewSearchItemDescription.setLayoutManager(new LinearLayoutManager(DescriptionSearchActivity.this));
+
+        editsearch = findViewById(R.id.search_item_by_description_store);
+        editsearch.setOnQueryTextListener(this);
+
+
     }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
+    }
+
+
+
 }
--- SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/DescriptionSearchAdapter.java
+++ SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/DescriptionSearchAdapter.java
@@ -1,19 +1,25 @@
 package com.gdipsa.iss.nus.sa46team1_adproject;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.gdipsa.iss.nus.sa46team1_adproject.Data.Item;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class DescriptionSearchAdapter extends RecyclerView.Adapter<DescriptionSearchAdapter.DescriptionSearchViewHolder> {
 
     private LayoutInflater mInflater;
 
-    List<Item> mItems;
+    private List<Item> mItems;
+    private ArrayList<Item> mItemsList = new ArrayList<Item>();
 
     class DescriptionSearchViewHolder extends RecyclerView.ViewHolder{
 
@@ -28,7 +34,52 @@ public class DescriptionSearchAdapter extends RecyclerView.Adapter<DescriptionSe
         }
     }
 
+    DescriptionSearchAdapter(Context context, List<Item> mItems){
+        mInflater = LayoutInflater.from(context);
+        this.mItems = mItems;
+        this.mItemsList.addAll(mItems);
 
+    }
+
+    @NonNull
+    @Override
+    public DescriptionSearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_search_description, parent, false);
+        return new DescriptionSearchViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DescriptionSearchViewHolder holder, int position) {
+
+        Item current = mItems.get(position);
+        holder.searchItemTextView.setText("Item: " + current.getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mItems != null)
+            return mItems.size();
+        else return 0;
+    }
+
+
+    // Filter Class
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        mItems.clear();
+        if (charText.length() == 0) {
+            mItems.addAll(mItemsList);
+        } else {
+            for (Item i : mItemsList) {
+                if (i.getDescription().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    mItems.add(i);
+                }
+            }
+        }
+
+        notifyDataSetChanged();
+    }
 
 
 
