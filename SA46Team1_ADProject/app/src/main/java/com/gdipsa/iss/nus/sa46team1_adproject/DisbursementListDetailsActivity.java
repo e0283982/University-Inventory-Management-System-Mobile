5c8f55651b2951cb1263821eacb9373a5370b233
Hendri <hendrisetiawardana@gmail.com>
2018-07-26 21:42:00 +0800

    checkpoint
    checkpoint

---
+++ SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/DisbursementListDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.gdipsa.iss.nus.sa46team1_adproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisbursementListDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disbursement_list_details);
+
+
+
+
+    }
+}
