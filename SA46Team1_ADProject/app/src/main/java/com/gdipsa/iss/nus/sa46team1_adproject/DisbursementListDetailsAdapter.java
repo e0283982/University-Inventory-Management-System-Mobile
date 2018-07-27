12de9a2b519d3f8c6e728bc674b09d396dff47f4
Hendri <hendrisetiawardana@gmail.com>
2018-07-27 11:29:08 +0800

    checkpoint
    checkpoint

--- SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/DisbursementListDetailsAdapter.java
+++ SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/DisbursementListDetailsAdapter.java
@@ -1,11 +1,15 @@
 package com.gdipsa.iss.nus.sa46team1_adproject;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
@@ -13,6 +17,7 @@ import com.gdipsa.iss.nus.sa46team1_adproject.Data.DisbursementList;
 import com.gdipsa.iss.nus.sa46team1_adproject.Data.DisbursementListDetail;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -28,6 +33,8 @@ public class DisbursementListDetailsAdapter extends RecyclerView.Adapter<Disburs
         private TextView disbursementDetailQtyOrdTextView;
         private TextView disbursementDetailQtyRecTextView;
 
+        private String itemDescription;
+        private int qtyReceived;
 
 
         public DisbursementListDetailsViewHolder(View itemView) {
@@ -45,7 +52,37 @@ public class DisbursementListDetailsAdapter extends RecyclerView.Adapter<Disburs
         @Override
         public void onClick(View v) {
 
-            //TODO: for adjustment on click
+            Context context = v.getContext();
+
+            final Dialog dialog = new Dialog(context);
+            dialog.setContentView(R.layout.dialog_adjust_quantity_received_disbursement);
+
+            TextView itemDescriptionTextView = dialog.findViewById(R.id.textView_dialog_adjust_qty_received_item_description);
+            itemDescriptionTextView.setText(itemDescription);
+
+            Button submitButton = dialog.findViewById(R.id.button_dialog_adjust_qty_received_submit);
+            submitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                    dialog.dismiss();
+                }
+            });
+
+            Spinner adjustSpinner = dialog.findViewById(R.id.spinner_dialog_adjust_qty_received);
+            List<Integer> quantitySpinner = new ArrayList<Integer>();
+            for (int i = 1; i <= qtyReceived; i++){
+                quantitySpinner.add(i);
+            }
+
+            ArrayAdapter<Integer> quantitySpinnerAdapter = new ArrayAdapter<Integer>(context, android.R.layout.simple_spinner_item, quantitySpinner);
+
+            quantitySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+            adjustSpinner.setAdapter(quantitySpinnerAdapter);
+
+            dialog.show();
 
         }
     }
@@ -72,6 +109,9 @@ public class DisbursementListDetailsAdapter extends RecyclerView.Adapter<Disburs
         holder.disbursementDetailUoMTextView.setText(current.getItemUoM());
         holder.disbursementDetailQtyOrdTextView.setText("" + current.getQtyOrdered());
         holder.disbursementDetailQtyRecTextView.setText("" + current.getQtyReceived());
+
+        holder.itemDescription = current.getItemDescription();
+        holder.qtyReceived = current.getQtyReceived();
 
     }
 
