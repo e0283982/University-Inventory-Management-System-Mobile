24944fa226dd2f53c2c74e451aacc48310ab0d35
Hendri <hendrisetiawardana@gmail.com>
2018-07-28 14:30:55 +0800

    added nav drawer for department user
    added nav drawer for department user

---
+++ SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/Department/NewRequisitionActivity.java
@@ -0,0 +1,17 @@
+package com.gdipsa.iss.nus.sa46team1_adproject.Department;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gdipsa.iss.nus.sa46team1_adproject.R;
+
+public class NewRequisitionActivity extends AppBaseDepartmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_requisition);
+
+
+    }
+}
