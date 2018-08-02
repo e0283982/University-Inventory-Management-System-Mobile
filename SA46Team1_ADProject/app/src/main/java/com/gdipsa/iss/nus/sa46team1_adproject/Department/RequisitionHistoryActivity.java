ea4cad8c5b55ffded3609a41e0f0f9221c45d881
Hendri <hendrisetiawardana@gmail.com>
2018-08-02 19:00:28 +0800

    added filter for retrieval
    added filter for retrieval

--- SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/Department/RequisitionHistoryActivity.java
+++ SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/Department/RequisitionHistoryActivity.java
@@ -104,7 +104,6 @@ public class RequisitionHistoryActivity extends AppBaseDepartmentActivity {
                 }
             });
 
-
         }
     }
 
--- SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/RetrievalListActivity.java
+++ SA46Team1_ADProject/app/src/main/java/com/gdipsa/iss/nus/sa46team1_adproject/RetrievalListActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.gdipsa.iss.nus.sa46team1_adproject.Data.StaffRequisitionHeader;
 import com.gdipsa.iss.nus.sa46team1_adproject.Data.StockRetrieval;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -22,8 +26,9 @@ public class RetrievalListActivity extends AppBaseActivity {
     private RetrievalListAdapter adapter;
     private ProgressBar progressBar;
     private TextView retrievalListDisbursedTextView;
-
     private String stoRetId;
+    private List<StockRetrieval> resultList;
+    private Spinner filterSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,7 @@ public class RetrievalListActivity extends AppBaseActivity {
         progressBar = findViewById(R.id.progressbar_retrieval_list);
         retrievalListDisbursedTextView = findViewById(R.id.textView_retrieval_list_disbursed);
 
+        filterSpinner = findViewById(R.id.spinner_retrieval_list_collection_point_filter);
     }
 
     private class MyTask extends AsyncTask<String, Void, List<StockRetrieval>>{
@@ -45,10 +51,44 @@ public class RetrievalListActivity extends AppBaseActivity {
         @Override
         protected void onPostExecute(List<StockRetrieval> result) {
 
+            resultList = result;
+
             progressBar.setVisibility(View.GONE);
             adapter = new RetrievalListAdapter(RetrievalListActivity.this, result, stoRetId);
             mRecyclerViewStockRetrievalList.setAdapter(adapter);
             mRecyclerViewStockRetrievalList.setLayoutManager(new LinearLayoutManager(RetrievalListActivity.this));
+
+            filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                    String selectedItem = parent.getItemAtPosition(position).toString();
+
+                    List<StockRetrieval> filterResult = new ArrayList<StockRetrieval>();
+
+                    for(StockRetrieval stockRetrieval : resultList){
+
+                        if (stockRetrieval.getCollectionPointDescription().equals(selectedItem)){
+                            filterResult.add(stockRetrieval);
+                        }
+
+                    }
+
+                    if (selectedItem.equals("All")){
+                        filterResult = resultList;
+                    }
+
+                    adapter = new RetrievalListAdapter(RetrievalListActivity.this, filterResult, stoRetId);
+                    mRecyclerViewStockRetrievalList.setAdapter(adapter);
+                    mRecyclerViewStockRetrievalList.setLayoutManager(new LinearLayoutManager(RetrievalListActivity.this));
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
 
         }
     }
